e3cbb41d6cbe465b050c2c07b0d20818d0b00f73
code-vuk97 <stefanovicvuk97@gmail.com>
2019-11-18 00:40:26 +0100

    Updates

---
+++ precache-manifest.e1497cb673d0c6ef4b4aa52b83e63173.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54004c059e95c2552dcb26a7486ec353",
+    "url": "/react-hangman/index.html"
+  },
+  {
+    "revision": "9f4dd0e61adf05c30897",
+    "url": "/react-hangman/static/css/main.9c981609.chunk.css"
+  },
+  {
+    "revision": "6787318a4e500822565e",
+    "url": "/react-hangman/static/js/2.9416f290.chunk.js"
+  },
+  {
+    "revision": "9f4dd0e61adf05c30897",
+    "url": "/react-hangman/static/js/main.3d0a1876.chunk.js"
+  },
+  {
+    "revision": "a9675aed196cf11b79b2",
+    "url": "/react-hangman/static/js/runtime-main.a0a04435.js"
+  }
+]);
